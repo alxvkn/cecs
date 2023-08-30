f64de7a6e40d83cfb5c183f55f12edff51b72f82
on8t <64454213+on8t@users.noreply.github.com>
2023-08-30 21:14:56 +0300

    fix message in movement system

--- test/test.c
+++ test/test.c
@@ -24,9 +24,11 @@ ECS_DEFINE_SYSTEM(movement , position_mask | velocity_mask) {
     struct velocity* v = (struct velocity*)ecs_get_component(ctx, velocity_mask, entity_id);
 
     printf("movement_system called with p = {\n"
-           "    x = %d\n\ty = %d\n"
+           "    x = %d\n"
+           "    y = %d\n"
            "} and v = {\n"
-           "    x = %d\n\ty = %d\n"
+           "    x = %d\n"
+           "    y = %d\n"
            "}\n",
            p->x, p->y, v->x, v->y);
 
