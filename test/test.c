06bfab00c209f2955e91f6d489abe7a76fb72033
on8t <64454213+on8t@users.noreply.github.com>
2024-04-26 00:28:23 +0300

    sdl test: utilize delta_time, vsync rendering, removed a lot of printing

--- test/test.c
+++ test/test.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
+#include <stdlib.h>
 #include <sys/types.h>
 
 #include <cecs.h>
@@ -20,23 +21,28 @@ struct velocity {
     float x, y;
 };
 
+const static ecs_component_mask_t mass_mask = velocity_mask << 1;
+struct mass {
+    float mass;
+};
+
 // SYSTEMS
 
 ECS_DEFINE_SYSTEM(movement , position_mask | velocity_mask) {
     struct position* p = (struct position*)ecs_get_component(ctx, position_mask, entity_id);
     struct velocity* v = (struct velocity*)ecs_get_component(ctx, velocity_mask, entity_id);
 
-    printf("movement_system called with p = {\n"
-           "    x = %f\n"
-           "    y = %f\n"
-           "} and v = {\n"
-           "    x = %f\n"
-           "    y = %f\n"
-           "}\n",
-           p->x, p->y, v->x, v->y);
+    // printf("movement_system called with p = {\n"
+    //        "    x = %f\n"
+    //        "    y = %f\n"
+    //        "} and v = {\n"
+    //        "    x = %f\n"
+    //        "    y = %f\n"
+    //        "}\n",
+    //        p->x, p->y, v->x, v->y);
 
-    p->x += v->x;
-    p->y += v->y;
+    p->x += v->x * delta_time;
+    p->y += v->y * delta_time;
 }
 
 // TODO: allow systems to have zero component mask
@@ -45,12 +51,25 @@ ECS_DEFINE_SYSTEM(debug, position_mask) {
     printf("debug system on entity with id %lu\n", entity_id);
 }
 
+ECS_DEFINE_SYSTEM(gravity, velocity_mask | mass_mask) {
+    struct velocity* v = (struct velocity*)ecs_get_component(ctx, velocity_mask, entity_id);
+    struct mass* m = (struct mass*)ecs_get_component(ctx, mass_mask, entity_id);
+
+    v->y += m->mass * delta_time;
+}
+
 SDL_Renderer* sdl_renderer = NULL;
 
 int init_sdl() {
     SDL_Window* window = NULL;
 
-    SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_SHOWN, &window, &sdl_renderer);
+    window = SDL_CreateWindow("hiii", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+
+    sdl_renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED
+    );
 
     SDL_ShowWindow(window);
     SDL_RenderClear(sdl_renderer);
@@ -60,7 +79,7 @@ int init_sdl() {
     return ECS_OK;
 }
 
-#define POINTS_TO_RENDER_COUNT_MAX 32
+#define POINTS_TO_RENDER_COUNT_MAX 2000
 static SDL_FPoint points_to_render[POINTS_TO_RENDER_COUNT_MAX];
 static int points_to_render_count = 0;
 static int add_point_to_render(SDL_FPoint point) {
@@ -77,14 +96,12 @@ void render_sdl() {
 
     SDL_SetRenderDrawColor(sdl_renderer, 255, 255, 255, 255);
 
-    for (int i = 0; i < points_to_render_count; i++) {
-        SDL_RenderDrawRectF(sdl_renderer, &(SDL_FRect){
-            .x = points_to_render[i].x,
-            .y = points_to_render[i].y,
-            .w = 10,
-            .h = 10,
-        });
-    }
+    SDL_RenderDrawPointsF(
+        sdl_renderer,
+        points_to_render,
+        points_to_render_count
+    );
+
     SDL_RenderPresent(sdl_renderer);
     points_to_render_count = 0; // clear
 }
@@ -98,34 +115,50 @@ ECS_DEFINE_SYSTEM(render, position_mask) {
     });
 }
 
-void handle_sdl_events() {
-}
-
 int main() {
     struct ecs_ctx ctx = {0};
 
     enum ecs_err err = ecs_init(&ctx,
         &(struct ecs_config) {
-            .entities_pool_size = 2,
+            .entities_pool_size = 2000,
             .systems_pool_size = 8,
-            .components_pool_pool_size = 2,
+            .components_pool_pool_size = 3,
         });
 
     ecs_register_component(&ctx, position_mask, sizeof(struct position));
     ecs_register_component(&ctx, velocity_mask, sizeof(struct velocity));
+    ecs_register_component(&ctx, mass_mask, sizeof(struct mass));
 
     ecs_register_system(&ctx, &movement);
-    ecs_register_system(&ctx, &debug);
+    // ecs_register_system(&ctx, &debug);
+    ecs_register_system(&ctx, &gravity);
 
     init_sdl();
 
     ecs_register_system(&ctx, &render);
 
+    srand(time(0));
+    for (int i = 0; i < 2000; i++) {
+        size_t e = ecs_add_entity(&ctx, position_mask | velocity_mask | mass_mask);
+        if (e == 0) {
+            printf("couldn't create entity at i = %d\n", i);
+            exit(0);
+        }
+
+        ((struct velocity*)ecs_get_component(&ctx, velocity_mask, e))->y = rand() % 50 - 50;
+        ((struct velocity*)ecs_get_component(&ctx, velocity_mask, e))->x = rand() % 10 - 5;
+
+        ((struct mass*)ecs_get_component(&ctx, mass_mask, e))->mass = 10;
+
+        ((struct position*)ecs_get_component(&ctx, position_mask, e))->x = rand() % 799;
+        ((struct position*)ecs_get_component(&ctx, position_mask, e))->y = 200;
+    }
+
     size_t e0 = ecs_add_entity(&ctx, position_mask | velocity_mask);
     size_t e1 = ecs_add_entity(&ctx, position_mask | velocity_mask);
 
-    ((struct velocity*)ecs_get_component(&ctx, velocity_mask, e0))->x = 1;
-    ((struct velocity*)ecs_get_component(&ctx, velocity_mask, e1))->x = 2;
+    ((struct velocity*)ecs_get_component(&ctx, velocity_mask, e0))->x = 50;
+    ((struct velocity*)ecs_get_component(&ctx, velocity_mask, e1))->x = 25;
 
     int quit = 0;
 
@@ -136,7 +169,7 @@ int main() {
         }
         ecs_run(&ctx);
         render_sdl();
-        usleep((1000 * 1000) / 60);
+        // usleep((1000 * 1000) / 60);
     }
 
     SDL_Quit();
